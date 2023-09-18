fe1829c8efdf1d53c16e56b0fbac085bf1812567
Yusuf-Moh <yusuf.moh255@outlook.com>
2023-09-18 22:31:29 +0200

    Beginning of the Dashboard

---
+++ Dashboard/dashboard.php
@@ -0,0 +1,30 @@
+<?php
+// Session Start and check for a current Login
+// Otherwise, you will get redirected to the login page
+include "../loginSystem/checkLogin.php";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <link rel="stylesheet" href="dashboard.css">
+    <!--Link to Material Icons-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
+</head>
+
+<body>
+    <header> <?php include '../NavBar/navbar.html'; ?></header>
+    <div class="container">
+        <!-- Content of the Dashboard -->
+        
+    </div>
+</body>
+
+</html>
